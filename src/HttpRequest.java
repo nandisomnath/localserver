db66554d5ce9c9b71a864cec3eb6e28e05ea9370
CodeOfSomnath <somnathnandi368@gmail.com>
2025-03-23 09:02:40 +0530

    handling the errors

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,47 +21,64 @@ public class HttpRequest {
         return headers.get(key);
     }
 
-    private String generateResponse(BufferedReader in) throws Exception {
-        // first line is the get request line
-        String line = in.readLine();
-        // This is the request info
-        System.out.printf("Request: %s\n", line);
-        String[] tokens;
-        tokens = line.split(" ");
-        String requestType = tokens[0].strip();
-        String requestPath = tokens[1].strip();
+    private String generateResponse(BufferedReader in) {
+        try {
+            // first line is the get request line
+            String line = in.readLine();
+            // This is the request info
+            System.out.printf("%s", line);
+            String[] tokens;
+            tokens = line.split(" ");
+            String requestType = tokens[0].strip();
+            String requestPath = tokens[1].strip();
 
-        File cwd = new File(".");
-        // File target = new File(String.format("%s%s%s", , File.pathSeparator,));
-        // FileReader reader = new FileReader(target);
-        // BufferedReader reader2;
-        Path path;
-        if (requestPath.equals("/")) {
-            path = Path.of(cwd.getCanonicalPath(), "index.html");
-        } else {
-            path =  Path.of(cwd.getCanonicalPath(), requestPath);
+            File cwd = new File(".");
+            // File target = new File(String.format("%s%s%s", , File.pathSeparator,));
+            // FileReader reader = new FileReader(target);
+            // BufferedReader reader2;
+            Path path;
+            if (requestPath.equals("/")) {
+                path = Path.of(cwd.getCanonicalPath(), "index.html");
+            } else {
+                path = Path.of(cwd.getCanonicalPath(), requestPath);
+            }
+            byte[] content = Files.readAllBytes(path);
+
+            String response = String.format("HTTP/1.1 200 OK\r\n\r\n%s", new String(content));
+
+            // TODO: parse this when all the headers are needed
+            // while (line != null) {
+            // line = in.readLine();
+            // System.out.println(line);
+            // }
+
+            return response;
+        } catch (IOException e) {
+            e.fillInStackTrace();
         }
-        byte[] content = Files.readAllBytes(path);
 
-        String response = String.format("HTTP/1.1 200 OK\r\n\r\n%s", new String(content));
+        StringBuilder content = new StringBuilder();
+        content.append("<html>");
+        content.append("<head><title>404 Not Found</title></head>");
+        content.append("<body>");
+        content.append("<h1>Not Found</h1>");
+        content.append("<p>The requested Url was Not found</p>");
+        content.append("</body>");
+        content.append("</html>");
 
-
-        // TODO: parse this when all the headers are needed
-        // while (line != null) {
-        //     line = in.readLine();
-        //     System.out.println(line);
-        // }
-
-        return response;
-
+        return String.format("HTTP/1.1 404 Not Found\r\n\r\n%s", content.toString());
     }
 
-    public void sendResponse(BufferedReader in, PrintWriter out) throws Exception {
+    public void sendResponse(BufferedReader in, PrintWriter out) {
 
         String response = this.generateResponse(in);
         // // printing the request string
         // System.out.println(new String(in.readLine()));
-
+        if (response.contains("200")) {
+            System.out.println("   200 OK");
+        } else {
+            System.out.println("   404 Not Found");
+        }
         out.printf(response);
     }
 
--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -8,28 +9,43 @@ public class HttpServer {
     private ServerSocket server;
     private int port;
 
-    public HttpServer(int port) throws Exception {
+    public HttpServer(int port) {
         this.port = port;
-        this.server = new ServerSocket(port);
+
+        try {
+            this.server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            server = null;
+        }
         System.out.printf("http://0.0.0.0:%d\n", server.getLocalPort());
     }
 
-    public void serve() throws Exception {
+    public void serve() {
+        // checking if the server is ready or not
+        if (this.server == null) {
+            return;
+        }
+
         Socket client;
         HttpRequest request;
         PrintWriter out;
         BufferedReader in;
 
         while (true) {
-            client = server.accept();
+            try {
+                client = server.accept();
 
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out = new PrintWriter(client.getOutputStream(), true);
 
-            request = new HttpRequest();
-            request.sendResponse(in, out);
+                request = new HttpRequest();
+                request.sendResponse(in, out);
 
-            client.close();
+                client.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
     }
